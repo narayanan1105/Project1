3f37f7ad8c5eed281d944bfdab3527af3f6cac96
narayanan1105 <narayanansoft11@gmail.com>
2017-08-05 07:25:06 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+for(int i=0;i<5;i++){
+	for(int j=0;j<a-i;j++){
+		System.out.print(" ");
+		
+	}
+	for(int k=0;k<(2*i-1);k++){
+		System.out.print("*");
+	}
+	System.out.println();	
+		
+}
+	}
+
+}
