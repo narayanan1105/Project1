2dcb7cda373bb108f918e7f9ac698a4d51e48795
narayanan1105 <narayanansoft11@gmail.com>
2017-08-18 08:11:40 +0530

    Add files via upload

---
+++ Test8.java
@@ -0,0 +1,31 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Test8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=11;
+		int s=a%2;
+		
+		
+switch( s)
+{
+case 0:
+	
+	System.out.println("Even number");
+	break;
+case 1:
+	
+	System.out.println("odd number");
+	break;
+default:
+	System.out.println("Empty value");
+	break;
+
+}
+	}
+
+}
+
