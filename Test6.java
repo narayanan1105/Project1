872ee229f0a78fa67f5cb74bab708ac5f4351894
narayanan1105 <narayanansoft11@gmail.com>
2017-08-18 08:09:47 +0530

    Add files via upload

---
+++ Test6.java
@@ -0,0 +1,33 @@
+package sample;
+
+public class Test6 {
+	 static int a=2;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		add();
+	}
+		
+		public static void add()
+		{
+			
+		int a=1;
+		
+		for(int i=0;i<=3;i++)
+			
+			
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print(a++ +" ");
+				
+			}
+			
+			System.out.println();
+		}
+			
+		}
+
+	}
+
+
