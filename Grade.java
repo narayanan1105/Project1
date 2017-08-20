4dc791f0b219ea630f069e815c6e916fec501baf
narayanan1105 <narayanansoft11@gmail.com>
2017-08-20 13:52:00 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,35 @@
+package sample;
+
+public class Grade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=61;
+if(a<=100 && a>90) {
+	System.out.println("A grade");
+	}
+else if(a>=81 && a<=90)
+{
+	System.out.println("B grade");
+}
+else if(a>=71 && a<=80)
+{
+	System.out.println("C grade");
+}
+else if(a>=61 && a<=70)
+{
+	System.out.println("D grade");
+
+}
+else if(a>=51 && a<=60)
+{
+	System.out.println("E grade");
+}
+else
+{
+	System.out.println("fail");
+}
+
+	}
+
+}
