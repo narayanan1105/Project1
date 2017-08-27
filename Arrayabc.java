4282cc511bf135c74dca13af514ac57da410aa8f
narayanan1105 <narayanansoft11@gmail.com>
2017-08-27 14:44:52 +0530

    Add files via upload

--- Arrayabc.java
+++ Arrayabc.java
@@ -5,11 +5,11 @@ public class Arrayabc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		char a[]={'A','B','C','D','E','F','G','H','I'};
-		for(int i=0,k=8;i<=4;i++,k--)
+		for(int i=0;i<=4;i++)
 		{
 		for(int j=0;j<=8;j++)
 			{
-				if(i==j||k==j)
+				if(i==j||i+j==8)
 				{
 				System.out.print(a[j]);
 				}
